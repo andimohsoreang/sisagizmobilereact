faea1be4051278f4d1a142181fd3ca4aa83a5d32
andimohsoreang <andimohamaddd@gmail.com>
2023-04-10 05:11:26 +0800

    random

--- App.js
+++ App.js
@@ -5,22 +5,12 @@ import Calculator from './backend/measurment/calculator'
 import Accumulation_report from './backend/report/accumulation_report'
 import Article from './backend/article/article'
 import PetugasLogin from './screens/PetugasLogin'
-import MainHomePosyandu from './screens/posyandu/home/index';
+import MainHomePosyandu from './screens/posyandu/home/MainHomePosyandu';
 export default function App() {
 
 
   return (
-    <View>
-      <MainHomePosyandu />
-    </View>
+   <MainHomePosyandu />
   )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ screens/posyandu/home/MainHomePosyandu.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function MainHomePosyandu() {
+  return (
+
+  <View style={{flex:1, backgroundColor:'#FFCE81'}}>
+  
+  </View>
+  )
+}
