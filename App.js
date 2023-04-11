23a5fd6e9104ef7486a3d27fca4840cef75935d4
zYuna646 <rangga_s1sisfo@mahsiswa.ung.ac.id>
2023-04-12 02:22:55 +0800

    set condition in home screen

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { ScrollView, StyleSheet, Text, View } from 'react-native'
-import { NavigationContainer} from '@react-navigation/native'
-import {createNativeStackNavigator} from '@react-navigation/native-stack'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import OnBoarding from './screens/OnBoarding/OnBoarding';
 import Article from './screens/Article/Article';
@@ -17,68 +17,82 @@ import PetugasLogin from './screens/PetugasLogin'
 import LoginScreenUser from './screens/Login';
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
+  const Tab = createBottomTabNavigator();
+  const [data, setData] = React.useState([])
 
+  React.useEffect(() => {
+    const fetchData = async () => {
+      const data_user = await _retrieve_data('data');
+      setData(data_user)
+    }
+    fetchData();
+  }, []);
 
-const Stack = createNativeStackNavigator();
-const Tab = createBottomTabNavigator();
+  function MyTabs() {
+    return (
+      <Tab.Navigator
+        screenOptions={{
+          tabBarShowLabels: false,
+          headerShown: false,
+        }}
+      >
+        <Tab.Screen
+          options={{
+            tabBarIcon: (props) => (
+              <Feat her name="home" size={24} color="black" />
+            ),
+          }}
+          name="Home"
+          component={HomeScreen}
+        />
+        <Tab.Screen
+          name="Pengukuran"
+          component={PetugasLogin}
+          options={{
+            tabBarIcon: (props) => (
+              <Feather name="aperture" size={24} color="black" />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Article"
+          component={Article}
+          options={{
+            tabBarIcon: (props) => (
+              <Feather name="book" size={24} color="black" />
+            ),
+          }}
+        />
 
-function MyTabs() {
-  return (
-    <Tab.Navigator
-      screenOptions={{
-        tabBarShowLabels: false,
-        headerShown: false,
-      }}
-    >
-      <Tab.Screen
-        options={{
-          tabBarIcon: (props) => (
-            <Feather name="home" size={24} color="black" />
-          ),
-        }}
-        name="Home"
-        component={HomeScreen}
-      />
-      <Tab.Screen
-        name="Pengukuran"
-        component={PetugasLogin}
-        options={{
-          tabBarIcon: (props) => (
-            <Feather name="aperture" size={24} color="black" />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Article"
-        component={Article}
-        options={{
-          tabBarIcon: (props) => (
-            <Feather name="book" size={24} color="black" />
-          ),
-        }}
-      />
+        {data_user != null ? (
 
-      <Tab.Screen
-        name="Profile"
-        component={Article}
-        options={{
-          tabBarIcon: (props) => (
-            <Feather name="user" size={24} color="black" />
-          ),
-        }}
-      />
-       <Tab.Screen
-        name="Login"
-        component={LoginScreenUser}
-        options={{
-          tabBarIcon: (props) => (
-            <Feather name="user" size={24} color="black" />
-          ),
-        }}
-      />
-    </Tab.Navigator>
-  );
-}
+          <Tab.Screen
+            name="Profile"
+            component={Article}
+            options={{
+              tabBarIcon: (props) => (
+                <Feather name="user" size={24} color="black" />
+              ),
+            }}
+          />
+
+        ) : (
+
+          <Tab.Screen
+            name="Login"
+            component={LoginScreenUser}
+            options={{
+              tabBarIcon: (props) => (
+                <Feather name="user" size={24} color="black" />
+              ),
+            }}
+
+          />
+        )}
+      </Tab.Navigator>
+    );
+  }
 
   return (
     <NavigationContainer>
